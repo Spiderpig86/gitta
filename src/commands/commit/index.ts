5851e26c5cd1d91682fe372ea110e8163ef0bd37
Stanley Lim <slim679975@gmail.com>
2021-09-02 01:03:07 -0700

    :sparkles: feat(commit): Add support for signed commits

--- src/commands/commit/index.ts
+++ src/commands/commit/index.ts
@@ -7,7 +7,6 @@ import { toList } from '../../utils/functions';
 import { Prompter } from '../prompts';
 
 export class CommitPrompter extends Prompter {
-
     protected async getPrompts() {
         const emojiModel = await this.emojiService.get();
 
@@ -19,17 +18,17 @@ export class CommitPrompter extends Prompter {
                 source: (answersSoFar: any[], input: string) => {
                     return Promise.resolve(
                         toList<EmojiModel, EmojiItemModel>(emojiModel)
-                        .filter((emoji) => {
-                            const emojiName = `${emoji.name}${emoji.description}`.toLowerCase();
-                            return !input || emojiName.indexOf(input.toLowerCase()) !== -1;
-                        })
-                        .map((emoji) => {
-                            return {
-                                name: `${emoji.emoji} ${chalk.blue(`:${emoji.name}:`)} - ${emoji.description}`,
-                                value: emoji,
-                            };
-                        })
-                    )
+                            .filter((emoji) => {
+                                const emojiName = `${emoji.name}${emoji.description}`.toLowerCase();
+                                return !input || emojiName.indexOf(input.toLowerCase()) !== -1;
+                            })
+                            .map((emoji) => {
+                                return {
+                                    name: `${emoji.emoji} ${chalk.blue(`:${emoji.name}:`)} - ${emoji.description}`,
+                                    value: emoji,
+                                };
+                            })
+                    );
                 },
             },
             ...(this.config.getIsUdacityStyleCommit()
@@ -62,25 +61,28 @@ export class CommitPrompter extends Prompter {
                     this.config.getEmojiFormat() === Constants.SETTINGS_EMOJI_FORMAT_MARKDOWN
                         ? emojiModel.code
                         : emojiModel.emoji;
-                
+
                 let formattedScope = ``;
                 if (this.config.getIsUdacityStyleCommit()) {
                     const scope = answers[Constants.COMMIT_SET_SCOPE_KEY];
                     if (scope) {
-                        formattedScope = `${emojiModel.type}(${answers[Constants.COMMIT_SET_SCOPE_KEY]}): `
+                        formattedScope = `${emojiModel.type}(${answers[Constants.COMMIT_SET_SCOPE_KEY]}): `;
                     } else {
-                        formattedScope = `${emojiModel.type}: `
+                        formattedScope = `${emojiModel.type}: `;
                     }
                 }
 
+                const isSigned = this.config.getSignCommit() ? [`-S`] : [];
                 const title = `${emoji} ${formattedScope}${answers[Constants.COMMIT_SET_TITLE_KEY]}`;
-                const message = answers[Constants.COMMIT_SET_MESSAGE_KEY];
+                const message = answers[Constants.COMMIT_SET_MESSAGE_KEY]
+                    ? [`-m`, answers[Constants.COMMIT_SET_MESSAGE_KEY]]
+                    : [];
 
                 if (this.config.getAddAll()) {
                     await execa(`git`, [`add`, `.`]);
                 }
 
-                const commitFlags = [`commit`, `-m`, title, ...(message ? [`-m`, message] : [])];
+                const commitFlags = [`commit`, ...isSigned, `-m`, title, ...message];
                 const { stdout } = await execa(`git`, commitFlags);
 
                 console.log(stdout);
